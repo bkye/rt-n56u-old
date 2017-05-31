77a339b19520cf487a680141f21c2779f6b03bc5
bkye <bkye@vip.qq.com>
2017-05-31 11:51:40 +0800

    K2硬改32+256+USB适配文件（未测试）

---
+++ trunk/configs/boards/PSG1218-256/board.h
@@ -0,0 +1,28 @@
+/* PHICOMM PSG1218 */
+
+#define BOARD_PID		"PSG1218-256"
+#define BOARD_NAME		"K2"
+#define BOARD_DESC		"PHICOMM K2 Wireless Router"
+#define BOARD_VENDOR_NAME	"PHICOMM"
+#define BOARD_VENDOR_URL	"http://www.phicomm.com/"
+#define BOARD_MODEL_URL		"http://www.phicomm.com/cn/index.php/Products/family_details/cateid/18/id/65.html"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	1
+#undef  BOARD_GPIO_BTN_WPS
+#define BOARD_GPIO_LED_ALL	10	/* 10: Blue, 11: Yellow */
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	8	/* 8: Red */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
