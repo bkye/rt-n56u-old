77da06d9aef76697eec1cdbd1800d4f53142bcf4
bkye <bkye@vip.qq.com>
2017-06-01 11:06:24 +0800

    fixed HC5661A

--- trunk/configs/boards/HC5661A/board.h
+++ trunk/configs/boards/HC5661A/board.h
@@ -1,8 +1,8 @@
-/* ASUS RT-AC1200 */
+/* HC5661A */
 
 #define BOARD_PID		"HC5661A"
 #define BOARD_NAME		"HC5661A"
-#define BOARD_DESC		"ASUS RT-AC1200 Wireless Router"
+#define BOARD_DESC		"HC5661A Wireless Router"
 #define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
 #define BOARD_VENDOR_URL	"http://www.asus.com/"
 #define BOARD_MODEL_URL		"http://www.asus.com/Networking/RT-AC1200/"
@@ -10,13 +10,13 @@
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	38
 #define BOARD_GPIO_LED_INVERTED
-#define BOARD_GPIO_BTN_WPS
-#define BOARD_GPIO_LED_ALL
-#undef  BOARD_GPIO_LED_WIFI     44
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI     44
 #define BOARD_GPIO_LED_POWER	37
 #undef  BOARD_GPIO_LED_LAN
-#undef  BOARD_GPIO_LED_WAN      1
-#define BOARD_GPIO_LED_USB
+#define BOARD_GPIO_LED_WAN      1
+#undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
 #undef  BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	0
